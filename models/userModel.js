1c12ebb9d35d7eff8bd1bb0dd86db3fd752f2056
Mert Özbolat <122216794+Mert-Ozbolat@users.noreply.github.com>
2025-04-14 16:49:23 +0300

    update

--- models/userModel.js
+++ models/userModel.js
@@ -52,7 +52,9 @@ const userSchema = new Schema({
     }
 })
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 12)
     this.passwordConfirm = undefined
     next()
 })
